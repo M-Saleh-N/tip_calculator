03053e4917af3a30a8c7fb856e12253c51cdc204
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-26 10:40:09 +0300

    set the conditions

--- src/components/calc.js
+++ src/components/calc.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./calc.css";
 
 function TipCalculator() {
-
+  const [purchase, setPurchase] = useState("");
+  const tip = purchase >= 1000 ? Math.floor(purchase / 1000) * 50 : 0;
+  const total = Number(purchase) + tip;
 }
 
